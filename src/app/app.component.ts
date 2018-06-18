2568d644342152253fd0149e6e21b9f5c06323d5
pablinchapin <pablinchapin@gmail.com>
2018-06-18 11:31:04 -0600

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+import { ToDoService } from './services/todo';
+import  ToDo  from './models/todo';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  
+  constructor(
+    private todoService : ToDoService
+  ){ }
+
+  public newTodo : ToDo = new ToDo();
+
+  todosList : ToDo[];
+  editTodos : ToDo[] = [];
+
+  ngOnInit(): void {
+
+    this.todoService.getTodos()
+      .subscribe(todos => {
+        this.todosList = todos;
+        console.log(todos);
+      });
+  }
+
+  create(){
+    this.todoService.createTodo(this.newTodo)
+      .subscribe((res) => {
+        this.todosList.push(res.data);
+        this.newTodo = new ToDo();
+      });
+  }
+
+  editTodo(todo : ToDo){
+    console.log(todo);
+
+    if(this.todosList.includes(todo)){
+      if(!this.editTodos.includes(todo)){
+        this.editTodos.push(todo);
+      }else{
+          this.editTodos.splice(this.editTodos.indexOf(todo), 1);
+          this.todoService.editTodo(todo)
+            .subscribe(res => {
+              console.log('Update successfully');
+            }, err => {
+              this.editTodo(todo);
+              console.error('Update failed');
+            });
+      }
+    }
+  }
+
+  doneTodo(todo : ToDo){
+    todo.status = 'Done';
+    this.todoService.editTodo(todo)
+      .subscribe(res => {
+        console.log('Update successfully');
+      }, err => {
+        this.editTodo(todo);
+        console.log('Update failed');
+      });
+  }
+
+
+  submitTodo(event, todo:ToDo){
+    if(event.keyCode === 13){
+      this.editTodo(todo);
+    }
+  }
+
+
+  deleteTodo(todo : ToDo){
+    this.todoService.deleteTodo(todo._id).subscribe(res => {
+      this.todosList.splice(this.todosList.indexOf(todo), 1);
+    })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ToDoService } from './services/todo';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -10,9 +15,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [ToDoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
